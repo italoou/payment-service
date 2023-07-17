c8c6f8736170aef8f023263c36108783b6c713f1
italo lima tre <italo.lima.residente@tre-rn.jus.br>
2023-07-17 18:23:58 -0300

    fix

--- src/main/java/br/jus/trern/payment/consumer/PaymentConsumer.java
+++ src/main/java/br/jus/trern/payment/consumer/PaymentConsumer.java
@@ -4,19 +4,15 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import br.jus.trern.payment.constants.RabbitMQConstants;
 import br.jus.trern.payment.model.PaymentInfo;
 import br.jus.trern.payment.service.PaymentService;
 
-@Component
+//@Component
 public class PaymentConsumer {
 
 	@Autowired
 	private PaymentService service;
 	
-	@RabbitListener(queues = "Local")
-	private void consumer(PaymentInfo info) {
-		
-		service.pay(info);
-		
-	}
+	
 }
